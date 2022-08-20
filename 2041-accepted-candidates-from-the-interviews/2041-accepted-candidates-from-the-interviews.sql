ef5fe7b69e9842a6808b1d60109443d74008ad6e
helenboit <97775374+helenboit@users.noreply.github.com>
2022-08-20 14:28:17 -0500

    Time: 1622 ms (38.52%), Space: 0B (100.00%) - LeetHub

--- 2041-accepted-candidates-from-the-interviews/2041-accepted-candidates-from-the-interviews.sql
+++ 2041-accepted-candidates-from-the-interviews/2041-accepted-candidates-from-the-interviews.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
 select  distinct candidate_id
-from    Candidates c join Rounds r
-        on c.interview_id = r.interview_id and years_of_exp>=2 and c.interview_id in
+from    Candidates
+where   years_of_exp >= 2 and interview_id in
         (select interview_id 
          from Rounds 
          group by interview_id 
