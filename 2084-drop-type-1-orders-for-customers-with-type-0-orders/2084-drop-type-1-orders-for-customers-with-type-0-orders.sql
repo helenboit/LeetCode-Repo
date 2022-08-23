a803eebb0fce99e5826a82e2fc539e1998cb5bb1
helenboit <97775374+helenboit@users.noreply.github.com>
2022-08-22 20:40:37 -0500

    Time: 596 ms (45.34%), Space: 0B (100.00%) - LeetHub

---
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select * from Orders where order_type = 0
+
+union 
+
+select * from Orders where customer_id not in
+(select customer_id from Orders where order_type = 0)
