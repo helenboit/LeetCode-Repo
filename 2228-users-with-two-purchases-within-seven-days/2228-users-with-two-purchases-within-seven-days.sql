a2a155588df864d516a6605d8d6ff3165d5e5b92
helenboit <97775374+helenboit@users.noreply.github.com>
2022-08-24 21:40:39 -0500

    Time: 807 ms (20.69%), Space: 0B (100.00%) - LeetHub

--- 2228-users-with-two-purchases-within-seven-days/2228-users-with-two-purchases-within-seven-days.sql
+++ 2228-users-with-two-purchases-within-seven-days/2228-users-with-two-purchases-within-seven-days.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
+/*
 select distinct p1.user_id
 from Purchases p1 join Purchases p2 
     on p1.user_id = p2.user_id 
@@ -8,3 +8,11 @@ from Purchases p1 join Purchases p2
 order by p1.user_id
 
 
+*/
+
+select  distinct user_id
+from
+( select user_id, purchase_date,
+lag(purchase_date) over (partition by user_id order by purchase_date) prev_purchase_date
+from purchases) t
+where datediff(purchase_date, prev_purchase_date) <=7
