9f301233062e0c255e9a9ebd1c3374cb907b0586
helenboit <97775374+helenboit@users.noreply.github.com>
2022-09-04 21:32:06 -0500

    Time: 1069 ms (42.81%), Space: 0B (100.00%) - LeetHub

--- 2346-compute-the-rank-as-a-percentage/2346-compute-the-rank-as-a-percentage.sql
+++ 2346-compute-the-rank-as-a-percentage/2346-compute-the-rank-as-a-percentage.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+
+/*
 with t as
 (
 select department_id, count(*) as cnt
@@ -10,3 +12,11 @@ group by department_id
 select student_id, Students.department_id, ifnull(round(100*(rank() over (partition by department_id order by mark desc)-1)/(cnt-1),2),0) as percentage
 from Students join t on Students.department_id = t.department_id
 
+*/
+
+SELECT student_id, department_id, 
+    ROUND(100*PERCENT_RANK() OVER (
+          PARTITION BY department_id 
+          ORDER BY mark DESC)
+    , 2) AS percentage 
+FROM Students
