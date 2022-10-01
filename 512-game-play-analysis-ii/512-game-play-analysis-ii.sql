bbcdda571754f29e016da19649b3f9626860f5d3
helenboit <97775374+helenboit@users.noreply.github.com>
2022-10-01 17:24:08 -0500

    Time: 763 ms (46.94%), Space: 0B (100.00%) - LeetHub

---
+++ 512-game-play-analysis-ii/512-game-play-analysis-ii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+with cte as
+(
+    select player_id, device_id, rank () over(partition by player_id order by event_date) as rnk
+    from Activity
+)
+
+select player_id, device_id
+from cte
+where rnk = 1
