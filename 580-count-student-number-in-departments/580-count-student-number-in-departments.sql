7cdf934eb1a8200fe522f942997f4a9553e18ac1
helenboit <97775374+helenboit@users.noreply.github.com>
2022-10-02 21:25:43 -0500

    Time: 898 ms (58.11%), Space: 0B (100.00%) - LeetHub

---
+++ 580-count-student-number-in-departments/580-count-student-number-in-departments.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select dept_name, ifnull(count(student_id),0) as student_number
+from Department d left join Student s on d.dept_id=s.dept_id 
+group by dept_name
+order by student_number desc, dept_name asc;
