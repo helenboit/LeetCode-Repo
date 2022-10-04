953177f2b1a103bb21156513ee57609bf3866b21
helenboit <97775374+helenboit@users.noreply.github.com>
2022-10-03 22:39:10 -0500

    Time: 486 ms (42.50%), Space: 0B (100.00%) - LeetHub

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -4,5 +4,5 @@
 
 select distinct c1.seat_id
 from Cinema c1, Cinema c2
-where ((c2.seat_id=c1.seat_id+1) or (c2.seat_id=c1.seat_id-1)) and c1.free=1 and c2.free=1
+where abs(c1.seat_id-c2.seat_id)=1 and c1.free=1 and c2.free=1
 order by c1.seat_id asc;
