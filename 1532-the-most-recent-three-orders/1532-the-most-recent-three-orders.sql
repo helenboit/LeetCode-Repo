8b7949e158cf25a5a94ca9f925f75b5f87b0c72b
helenboit <97775374+helenboit@users.noreply.github.com>
2022-11-01 21:42:38 -0500

    Time: 1399 ms (39.45%), Space: 0B (100.00%) - LeetHub

--- 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -1,17 +1,13 @@
+with new_t as
+(
+select name as customer_name, order_id, order_date, c.customer_id, row_number() over (partition by customer_id order by order_date desc) as rnk
+from Customers c join Orders o on c.customer_id=o.customer_id
+)
 
 
-select  customer_name,
-        customer_id,
-        order_id,
-        order_date
-from(
-select  c.customer_id,
-        name as customer_name,
-        order_id,
-        order_date,
-        rank() over (partition by c.customer_id order by customer_id asc, order_date desc) as rnk
-from Customers c left join Orders o on c.customer_id=o.customer_id
-where order_id is not null
-)t
+
+
+select customer_name, customer_id, order_id, order_date
+from new_t
 where rnk<=3
 order by customer_name, customer_id, order_date desc;
